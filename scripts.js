35d88031a9b066d674d7dec9ebb68e019ad11ad4
Jyri Konttinen <62062679+Jyri-droid@users.noreply.github.com>
2021-12-03 21:48:00 +0200

    Added Search

--- scripts.js
+++ scripts.js
@@ -26,57 +26,87 @@ getLuminance = (hex) => {
     return a[0] * 0.2126 + a[1] * 0.7152 + a[2] * 0.0722;
 }
 
-// Function to build a color swatch
+// Function to build one single color swatch
 addColorSwatch = (colorName, textColor) => {
+    // Create & style element
     const colorSwatch = document.createElement("button");
     colorSwatch.classList.add("colorSwatch");
     colorSwatch.style.backgroundColor = colorName;
     colorSwatch.style.color = textColor;
     colorSwatch.innerHTML = colorSwatch.style.backgroundColor;
+    // Add event listeners
+    colorSwatch.addEventListener("mouseover", function() {
+        colorSwatch.innerHTML = "<span class='material-icons'>content_copy</span>&nbsp;" + toHex(colorSwatch.style.backgroundColor);
+    });
+    colorSwatch.addEventListener("mouseleave", function() {
+        colorSwatch.innerHTML = colorSwatch.style.backgroundColor;
+    });
+    colorSwatch.addEventListener("click", function() {
+        const hexColor = toHex(colorSwatch.style.backgroundColor);
+        const hexWithoutHashtag = hexColor.match(/\w/g).join("");
+        navigator.clipboard.writeText(hexWithoutHashtag);
+        showToaster("done", "Hex value copied to clipboard!");
+    });
+    // Append element to DOM
     document.getElementById("htmlColors").appendChild(colorSwatch);
+}
+
+// Create a function to convert color name to hex
+toHex = (color) => wordToHex[color.toLowerCase()];
+
+// Create a function to remove all children of a node
+removeAllChildNodes = (parent) => {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+// Create a swatch for each color in an array
+renderSwatches = (array) => {
+    // Clear old swatches
+    removeAllChildNodes(document.getElementById("htmlColors"));
+    // Render new swatches
+    for (let i = 0; i < array.length; i++) {
+        // Get color name from JS object
+        const colorName = array[i];
+        // Decide text color based on luminosity
+        let textColorBasedOnLuminance;
+        const luminance = getLuminance(toHex(colorName));
+        if (luminance > .35) {
+            textColorBasedOnLuminance = "rgb(50,50,50)";
+        } else {
+            textColorBasedOnLuminance = "white";
+        }
+        // Create swatch
+        addColorSwatch(colorName, textColorBasedOnLuminance);
+    }
 }
 
 // Create an array with keys to the html color name object
 const colorKeys = Object.keys(wordToHex);
 
-// Create a function to convert color name to hex
-toHex = (color) => wordToHex[color.toLowerCase()];
+// Render all swatches in the initial view
+renderSwatches(colorKeys);
 
-// Create a swatch from each color
-for (let i = 0; i < colorKeys.length; i++) {
-    // Get color name from JS object
-    const colorName = colorKeys[i];
-    // Decide text color based on luminosity
-    let textColorBasedOnLuminance;
-    const luminance = getLuminance(toHex(colorName));
-    if (luminance > .35) {
-        textColorBasedOnLuminance = "rgb(50,50,50)";
-    } else {
-        textColorBasedOnLuminance = "white";
+// Make a search through all color names
+getColors = (searchWord) => {
+    const pattern = new RegExp(searchWord, "i");
+    let searchResults = [];
+    for (let i = 0; i < colorKeys.length; i++) {
+        if (pattern.test(colorKeys[i])) {
+            searchResults.push(colorKeys[i]);
+        }
     }
-    // Create swatch
-    addColorSwatch(colorName, textColorBasedOnLuminance);
+    return searchResults;
 }
 
-// Swatch hover and click events
-const allSwatches = document.getElementsByClassName("colorSwatch");
-for (let j of allSwatches) {
-    // Hover
-    j.addEventListener("mouseover", function() {
-        j.innerHTML = "<span class='material-icons'>content_copy</span>&nbsp;" + toHex(j.style.backgroundColor);
-    });
-    // Hover end
-    j.addEventListener("mouseleave", function() {
-        j.innerHTML = j.style.backgroundColor;
-    });
-    // Click
-    j.addEventListener("click", function() {
-        const hexColor = toHex(j.style.backgroundColor);
-        const hexWithoutHashtag = hexColor.match(/\w/g).join("");
-        navigator.clipboard.writeText(hexWithoutHashtag);
-        showToaster("done", "Hex value copied to clipboard!");
-    });
-}
+// Render search result
+const inputElement = document.getElementById("search");
+inputElement.addEventListener("input", function() {
+    const currentValue = inputElement.value;
+    const foundColors = getColors(currentValue);
+    renderSwatches(foundColors);
+});
 
 // Show toaster
 const toaster = document.getElementsByClassName("toaster");
